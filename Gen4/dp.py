5161ac58d3b06d713fe24c4d07cff93a09d0a91d
Admiral-Fish <admiralfish420@gmail.com>
2022-03-07 20:14:00 -0700

    Push initial location name modifiers

--- Gen4/dp.py
+++ Gen4/dp.py
@@ -48,7 +48,7 @@ def encounters():
     with open("pearl.bin", "wb+") as f:
         f.write(p)
 
-    with open("dp_en.txt", "w+", encoding="utf-8") as f:
+    with open("dppt_en.txt", "w+", encoding="utf-8") as f:
         map_names.sort(key=lambda x: (x[1], x[0]))
         for num, name in map_names:
             f.write(f"{num},{name}\n")
--- Gen4/pt.py
+++ Gen4/pt.py
@@ -1,7 +1,6 @@
 import os
 
 from .narc import Narc
-from .text import read_map_names
 
 SCRIPT_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,7 +8,6 @@ SCRIPT_FOLDER = os.path.dirname(os.path.abspath(__file__))
 def encounters():
     Pt_ENCOUNTERS = Narc(f"{SCRIPT_FOLDER}/pt/pl_enc_data.narc").get_elements()
     MAP_HEADERS = f"{SCRIPT_FOLDER}/pt/mapheaders.bin"
-    MAP_NAMES = read_map_names(f"{SCRIPT_FOLDER}/pt/mapnames.bin")
 
     with open(MAP_HEADERS, "rb") as f:
         map_headers = []
@@ -22,15 +20,6 @@ def encounters():
         encounter_id = map_header[14] | (map_header[15] << 8)
         if encounter_id != 65535:
             location_number = map_header[18]
-            location_name = MAP_NAMES[location_number]
-
-            map_name = (location_number, location_name)
-            if map_name not in map_names:
-                map_names.append(map_name)
-            else:
-                count = sum([1 for _, name in map_names if name == location_name])
-                location_number |= (count << 8)
-                map_names.append((location_number, location_name))
 
             # Platinum
             pt += location_number.to_bytes(2, "little")
@@ -38,8 +27,3 @@ def encounters():
 
     with open("platinum.bin", "wb+") as f:
         f.write(pt)
-
-    with open("pt_en.txt", "w+", encoding="utf-8") as f:
-        map_names.sort(key=lambda x: (x[1], x[0]))
-        for num, name in map_names:
-            f.write(f"{num},{name}\n")
